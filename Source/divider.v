1cae43e67425d7d37e4e220cdae483a4c15b58c9
suoglu <suoglu@icloud.com>
2020-10-04 11:10:01 +0200

    State transactions added

--- Source/divider.v
+++ Source/divider.v
@@ -2,14 +2,14 @@
 //divider module
 // dividend = divisor * quotient + remainder
 module divider_8bit(clk, rst, strt, dividend, divisor, quotient, remainder, not_valid, idle);
-    parameter IDLE = 2'b00, PRECALC = 2'b01, CALC = 2'b11;
+    parameter IDLE = 2'b00, PRECALC = 2'b01, CALC = 2'b11, POSTCALC = 2'b10;
     input clk, rst, strt;
     input [7:0] dividend, divisor;
     output reg [7:0] quotient, remainder;
     output not_valid; //True if divison result is not valid
     output idle; //True when ready to
     reg [8:0] dividend_reg, divisor_reg; //extra bit for sign check
-    wire update_divident; //update divident value after test substraction
+    wire update_divident; //update dividend value after test substraction, little typo 
     wire sign_of_test_sub; //0 for positive, 1 for negative
     wire [8:0] test_sub_res; //result of test substraction
     reg [1:0] state; //00: idle, 01: precalculate, 11: calcuate
@@ -21,13 +21,68 @@ module divider_8bit(clk, rst, strt, dividend, divisor, quotient, remainder, not_
     assign sign_of_test_sub = test_sub_res[8];
     assign update_divident = sign_of_test_sub;
 
+    //state transactions
+    always@(posedge clk or posedge rst)
+        begin
+            if(rst)
+                begin
+                    state <= IDLE;
+                end
+            else
+                begin
+                    case(state)
+                        IDLE:
+                            begin
+                                if(strt)
+                                    begin
+                                        state <= (divisor[7]) ? CALC : PRECALC;
+                                    end
+                            end
+                        PRECALC:
+                            begin
+                                state <= (divisor_reg[6]) ? CALC : PRECALC;
+                            end
+                        CALC:
+                            begin
+                               state <= (q_index == 3'd0) ? POSTCALC : CALC; 
+                            end
+                        POSTCALC:
+                            begin
+                                state <= IDLE;
+                            end
+                    endcase
+                    
+                end
+        end
+
+    //divisor register
+    always@(posedge clk)
+        begin
+            case(state)
+                IDLE:
+                    begin
+                        divisor_reg <= {1'b0, divisor};
+                    end
+                PRECALC:
+                    begin
+                        divisor_reg <= divisor_reg << 1;
+                    end
+                CALC:
+                    begin
+                        divisor_reg <= divisor_reg >> 1;
+                    end
+            endcase
+        end
+
     //handle reminder
     always@(posedge clk)
         begin //remaining number at divident is the remmainder
-            remainder <= (idle) ? remainder : dividend_reg[7:0]; 
+            remainder <= (state == POSTCALC) ? dividend_reg[7:0] : remainder; 
         end
     
 
+    
+    
     //handle dividend_reg
     always@(posedge clk)
         begin
